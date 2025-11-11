d2c930ee98c294a6b17e118340f6b80eedd4c2ca
Martin Thomas <martin.thomas@cariad.technology>
2025-11-11 10:49:40 +0100

    update

--- Lab2-Create-Functions-with-MongoDB/solution/createapp.sh
+++ Lab2-Create-Functions-with-MongoDB/solution/createapp.sh
@@ -2,7 +2,7 @@
 
 # Variablen
 RESOURCE_GROUP="udacitydemo"
-APP_NAME="MyTestAPIPython1235"
+APP_NAME="MyTestAPIPython39"
 LOCATION="westus"
 STORAGE_ACCOUNT="udacitydemo52025"
 PYTHON_VERSION="3.9"
@@ -10,13 +10,13 @@ PYTHON_VERSION="3.9"
 echo "🔧 Erstelle Azure Function App mit Python $PYTHON_VERSION..."
 
 # Schritt 1: Function App erstellen
-# az functionapp create \
-#   --resource-group $RESOURCE_GROUP \
-#   --consumption-plan-location $LOCATION \
-#   --name $APP_NAME \
-#   --storage-account $STORAGE_ACCOUNT \
-#   --runtime python \
-#   --os-type Linux
+az functionapp create \
+  --resource-group $RESOURCE_GROUP \
+  --consumption-plan-location $LOCATION \
+  --name $APP_NAME \
+  --storage-account $STORAGE_ACCOUNT \
+  --runtime python \
+  --os-type Linux
 
 # Schritt 2: Python-Version und Runtime konfigurieren
 echo "⚙️ Konfiguriere Python-Version und Functions Runtime..."
